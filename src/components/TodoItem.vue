6d86132e58511e2324ab736d6d68f0993466f26e
Vadim Valunskii <dvvv09@gmail.com>
2019-11-24 15:00:06 +0300

    feat: add scoped styles to components

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,7 +5,7 @@
             <strong>{{todoitem.id}}</strong>
             {{todoitem.title}}
         </span>
-        <button>&times;</button>
+        <button class="deletebtn">&times;</button>
     </li>
 </template>
 
@@ -19,3 +19,29 @@
         }
     }
 </script>
+
+<style scoped>
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+    }
+
+    .deletebtn {
+        background: red;
+        color: #fff;
+        border-radius: 50%;
+        border: none;
+        font-weight: bold;
+    }
+
+    input {
+        margin-right: 1rem;
+    }
+
+    .done{
+        text-decoration: line-through;
+    }
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -19,3 +19,11 @@
         }
     }
 </script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
