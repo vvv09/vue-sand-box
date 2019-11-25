d00c919e9a3b1d317560a1d5820c0f3e7e384883
Vadim Valunskii <dvvv09@gmail.com>
2019-11-25 07:04:27 +0300

    feat: add router

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h4>vue-приложение для управления тудушками</h4>
+        <p>которое при старте тащит записи с фейкового сервиса https://jsonplaceholder.typicode.com/
+            ...просто потому что умеет...</p>
+        <router-link to="/todos">Go!</router-link>
+    </div>
+</template>
